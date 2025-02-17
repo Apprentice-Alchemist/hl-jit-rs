8d615784b3f573311c786b1b45d05e15fac54f40
Apprentice-Alchemist <53486764+Apprentice-Alchemist@users.noreply.github.com>
2025-02-17 21:23:42 +0100

    Progress on defining types.

--- src/jit.rs
+++ src/jit.rs
@@ -4,11 +4,11 @@ use std::mem::offset_of;
 
 use cranelift::codegen::Context;
 use cranelift::jit::{JITBuilder, JITModule};
-use cranelift::module::{DataDescription, DataId, FuncId, Linkage, Module};
+use cranelift::module::{DataDescription, DataId, FuncId, Linkage, Module, ModuleError};
 use cranelift::prelude::*;
 
 use crate::code::{FunIdx, HLType, StrIdx, TypeIdx, UStrIdx};
-use crate::sys::{hl_type, hl_type_kind};
+use crate::sys::{hl_type, hl_type_fun, hl_type_kind};
 
 #[derive(Default)]
 struct Indexes {
@@ -36,9 +36,6 @@ pub struct JitCtx<M: Module> {
 
 impl<M: Module> JitCtx<M> {
     pub fn new(m: M, code: crate::code::Code) -> Self {
-        // let jit_b = JITBuilder::new(cranelift::module::default_libcall_names()).unwrap();
-        // let jit_m = JITModule::new(jit_b);
-        // jit_m.
         let ctx = m.make_context();
         Self {
             m,
@@ -116,6 +113,50 @@ impl<M: Module> JitCtx<M> {
         Ok(())
     }
 
+    fn write_data(m: &M, data: &mut DataDescription, data_id: DataId, offset: usize) {
+        let val = m.declare_data_in_data(data_id, data);
+        data.write_data_addr(offset.try_into().unwrap(), val, 0);
+    }
+
+    fn build_type_arr(m: &mut M, idxs: &Indexes, arr: &[TypeIdx]) -> Result<DataId, ModuleError> {
+        let id = m.declare_anonymous_data(true, false)?;
+        let size = size_of::<*mut hl_type>() * arr.len();
+        let align = align_of::<*mut hl_type>();
+        let mut data = DataDescription::new();
+        data.set_align(align as u64);
+        data.define_zeroinit(size);
+        for (pos, ty) in arr.iter().enumerate() {
+            Self::write_data(
+                m,
+                &mut data,
+                idxs.types[ty],
+                size_of::<*mut hl_type>() * pos,
+            );
+        }
+        Ok(id)
+    }
+
+    fn build_type_fun(
+        m: &mut M,
+        idxs: &Indexes,
+        args: &[TypeIdx],
+        ret: &TypeIdx,
+    ) -> Result<DataId, ModuleError> {
+        let id = m.declare_anonymous_data(true, false)?;
+        let mut buf: Vec<u8> = vec![0u8; size_of::<hl_type_fun>()];
+        let nargs: std::ffi::c_int = args.len().try_into().unwrap();
+        buf[offset_of!(hl_type_fun, nargs)
+            ..offset_of!(hl_type_fun, nargs) + size_of::<std::ffi::c_int>()]
+            .copy_from_slice(&nargs.to_ne_bytes());
+        let mut data = DataDescription::new();
+        data.set_align(align_of::<hl_type_fun>() as u64);
+        data.define(buf.into_boxed_slice());
+        Self::write_data(m, &mut data, idxs.types[ret], offset_of!(hl_type_fun, ret));
+        let arr_id = Self::build_type_arr(m, idxs, args)?;
+        Self::write_data(m, &mut data, arr_id, offset_of!(hl_type_fun, args));
+        Ok(id)
+    }
+
     pub fn define_types(&mut self) -> Result<(), Box<dyn Error>> {
         for (pos, ty) in self.code.types.iter().enumerate() {
             let id = self.m.declare_anonymous_data(true, false)?;
@@ -126,47 +167,49 @@ impl<M: Module> JitCtx<M> {
             buf[kind_offset..kind_offset + size_of::<hl_type_kind>()]
                 .copy_from_slice(&ty.type_kind().to_ne_bytes());
             data.define(buf.into_boxed_slice());
-            match ty {
-                HLType::Void => (),
-                HLType::UInt8 => (),
-                HLType::UInt16 => (),
-                HLType::Int32 => (),
-                HLType::Int64 => (),
-                HLType::Float32 => (),
-                HLType::Float64 => (),
-                HLType::Boolean => (),
-                HLType::Bytes => (),
-                HLType::Dynamic => (),
-                HLType::Function { args, ret } => todo!(),
+            let data_id = match ty {
+                HLType::Void => None,
+                HLType::UInt8 => None,
+                HLType::UInt16 => None,
+                HLType::Int32 => None,
+                HLType::Int64 => None,
+                HLType::Float32 => None,
+                HLType::Float64 => None,
+                HLType::Boolean => None,
+                HLType::Bytes => None,
+                HLType::Dynamic => None,
+                HLType::Function { args, ret } => {
+                    Some(Self::build_type_fun(&mut self.m, &self.idxs, args, ret)?)
+                }
                 HLType::Object(type_obj) => todo!(),
-                HLType::Array => (),
-                HLType::Type => (),
-                HLType::Reference(type_idx) => todo!(),
+                HLType::Array => None,
+                HLType::Type => None,
+                HLType::Reference(type_idx) => Some(self.idxs.types[type_idx]),
                 HLType::Virtual { fields } => todo!(),
-                HLType::Dynobj => (),
-                HLType::Abstract(ustr_idx) => todo!(),
+                HLType::Dynobj => None,
+                HLType::Abstract(ustr_idx) => Some(self.idxs.ustr[ustr_idx]),
                 HLType::Enum {
                     name,
                     global_value,
                     constructs,
                 } => todo!(),
-                HLType::Null(type_idx) => {
-                    let data_id = self.idxs.types[type_idx];
-                    let val = self.m.declare_data_in_data(data_id, &mut data);
-                    let offset = offset_of!(hl_type, __bindgen_anon_1);
-                    data.write_data_addr(offset.try_into().unwrap(), val, 0);
-                }
+                HLType::Null(type_idx) => Some(self.idxs.types[type_idx]),
                 HLType::Method { args, ret } => todo!(),
                 HLType::Struct(type_obj) => todo!(),
-                HLType::Packed(type_idx) => todo!(),
-                HLType::Guid => (),
+                HLType::Packed(type_idx) => Some(self.idxs.types[type_idx]),
+                HLType::Guid => None,
+            };
+
+            if let Some(data_id) = data_id {
+                Self::write_data(
+                    &self.m,
+                    &mut data,
+                    data_id,
+                    offset_of!(hl_type, __bindgen_anon_1),
+                )
             }
-            // TODO: hl_type_obj etc
-
             self.m.define_data(id, &data)?;
-            // self.idxs.types.insert(TypeIdx::from_usize(pos), id);
         }
-
         Ok(())
     }
 }
